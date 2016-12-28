dd3b4270d494f816fe5865c93d81d3872914f3fb
Дмитрий Соловьёв <md.65@mail.ru>
2016-12-29 02:57:11 +0300

    Update Laboratory-3.cpp

--- Laboratory-3.cpp
+++ Laboratory-3.cpp
@@ -6,25 +6,23 @@
 using namespace std;
 
 int main()
-{
-	int k = 0;
-	int sign = 1;
-	double sum_mod = 0.0;
-	double sum_ = 0.0;
-	double eps = 1e-4;
-
-	cout << " Sum where k-> infinity = (-1)^k+1/k^3" << endl;
-
-	do
-	{
-	   k++;
-	   sum_mod = (double)1/(k*k*k);
-	   sum_ +=sign*sum_mod;
-	   sign *= -1;
-	}
-	while (sum_mod>=eps);
-	cout << "Sum= " << sum_<< endl;
-	cout << "k= " << k << endl;
-	_getch();
-	return 0;
+{	
+int k = 0;	
+int sign = 1;	
+double sum_mod = 0.0;	
+double sum_ = 0.0;	
+double eps = 1e-4;	
+cout << " Sum where k-> infinity = (-1)^k+1/k^3" << endl;	
+do	
+{	
+   k++;	
+   sum_mod = (double)1/(k*k*k);	
+   sum_ +=sign*sum_mod;	
+   sign *= -1;	
+}	
+while (sum_mod>=eps);	
+cout << "Sum= " << sum_<< endl;	
+cout << "k= " << k << endl;	
+_getch();	
+return 0;
 }
